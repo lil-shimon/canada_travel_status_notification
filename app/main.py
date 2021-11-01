738c592b2596376a407911dd1a1ec095478102a5
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-11-01 17:25:29 +0900

    refactor(file): create get_status.py

--- app/main.py
+++ app/main.py
@@ -1,21 +1,4 @@
-import requests
-import re
-from bs4 import BeautifulSoup
-
-def get_status_handler():
-  url = 'https://www.ca.emb-japan.go.jp/itpr_ja/Covid19_20200330.html'
-  res = requests.get(url)
-
-  ## 文字化け直す
-  res.encoding = res.apparent_encoding
-
-  soup = BeautifulSoup(res.text, "html.parser")
-  # elems = soup.find_all(href=re.compile("aly"))
-  elems = soup.find_all('span')
-
-  ## カナダ全土のレベルステータス
-  canada_travel_status = elems[16].contents[0]
-  print(canada_travel_status)
+import get_status
 
 if __name__ == "__main__":
-  get_status_handler()
+  get_status.get_status_handler()
